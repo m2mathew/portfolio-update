cd901124a09c4485f5f9ce789f1a104722d35546
Mike Mathew <m2mathew@me.com>
2016-11-01 23:48:15 -0500

    Add backbone and remove class names

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Title from './shared/title';
 
 class App extends Component {
   render() {
     return (
-      <Title />
+      <main>
+        <Title />
+      </main>
     );
   }
 }
